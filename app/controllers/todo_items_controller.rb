d7747ca66bfae4efcc7de55f8f3f071bd1bc9173
Aileen Tran <aileen.tran27@gmail.com>
2020-11-23 18:05:41 -0800

    Edited controllers to include notifications for todo_items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,13 +4,21 @@ class TodoItemsController < ApplicationController
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
-     redirect_to @todo_list
+     respond_to do |format|
+       if @todo_item.save
+         format.html { redirect_to @todo_list, notice: 'Todo item was successfully created.' }
+         format.json { render :show, status: :created, location: @todo_list }
+       end
+     end
   end
 
   def destroy
     @todo_item = TodoItem.find(params[:id])
     @todo_item.destroy
-    redirect_to todo_list_path(@todo_list)
+    respond_to do |format|
+      format.html { redirect_to todo_lists_url, notice: 'Todo item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   def edit
@@ -19,7 +27,10 @@ class TodoItemsController < ApplicationController
   def update
     @todo_item.content = params["post"]["content"]
     @todo_item.save
-    redirect_to todo_list_path(@todo_list)
+    respond_to do |format|
+      format.html { redirect_to @todo_list, notice: 'Todo item was successfully updated.' }
+      format.json { render :show, status: :ok, location: @todo_list }
+    end
   end
 
   def complete
