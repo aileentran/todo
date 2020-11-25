ab47d73422f37dfd55696f015fb15ed959b9bd71
Aileen Tran <aileen.tran27@gmail.com>
2020-11-25 13:20:22 -0800

    Adjusted spacing and input boxes

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,6 +2,7 @@ class TodoItemsController < ApplicationController
   before_action :set_todo_list
   before_action :set_todo_item, except: [:create]
 
+# TODO: figure out errors notifications?
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
      respond_to do |format|
